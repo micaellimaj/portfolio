38274570d0a4f28897d3a7d98d2ecce3a5436af5
Micael Lima <131397330+micaellimaj@users.noreply.github.com>
2026-03-18 21:02:16 -0300

    + ajustes

--- Typefolio-1.0.0/next.config.ts
+++ Typefolio-1.0.0/next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
   },
 };
 
